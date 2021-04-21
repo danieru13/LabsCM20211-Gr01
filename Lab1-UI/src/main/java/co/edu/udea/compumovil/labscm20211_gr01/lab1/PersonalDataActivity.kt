739570c69073f4dbb57c8cf256b28f016934621d
Daniel Vanegas <danieru1313@gmail.com>
2021-04-21 00:29:55 -0500

    Create empty activity

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labscm20211_gr01/lab1/PersonalDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.labscm20211_gr01.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+    }
+}
